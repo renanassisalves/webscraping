9c8a19949eb7e38549bbc50d477d62cb42bb81c6
Renan de Assis Alves <renan.assis.alves@hotmail.com>
2022-08-21 22:37:21 -0300

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='webscraping_training',
+    name='webscraping',
     version='1.0',
     packages=[''],
     url='',
     license='PSFL',
     author='Renan Assis',
     author_email='Renan Assis',
-    description='Webscraping training using scrapethissite.com'
+    description='Python webscraping training using scrapethissite.com'
 )
